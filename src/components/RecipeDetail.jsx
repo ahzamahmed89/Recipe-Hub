415d54228d7a5a14c928e51b27283e8f70a8345f
ahzamahmed89 <153096034+ahzamahmed89@users.noreply.github.com>
2024-07-09 23:01:10 +0500

    Update RecipeDetail.jsx

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import sampleRecipes from './sampleRecipes.js';
+import sampleRecipes from '../sampleRecipes.js';
 
 const RecipeDetail = () => {
   const { id } = useParams();
